16aa9b83878fcada56d0c59ca3dabe002a9c94c6
Kayden Cooper <kayden_cooper@yahoo.com>
2022-11-02 19:21:47 -0600

    build

--- cooper-cookbook.client/src/AppState.js
+++ cooper-cookbook.client/src/AppState.js
@@ -6,88 +6,88 @@ export const AppState = reactive({
   recipe: [],
   categories: [
     {
-      url: '../src/assets/img/appetizer.jpg',
+      url: require('../src/assets/img/appetizer.jpg'),
       name: 'APPETIZERS'
     },
     {
-      url: '../src/assets/img/bread.jpg',
+      url: require('../src/assets/img/bread.jpg'),
       name: 'BREAD'
     },
     {
-      url: '../src/assets/img/muffin.jpg',
+      url: require('../src/assets/img/muffin.jpg'),
       name: 'MUFFINS'
     },
     {
-      url: '../src/assets/img/pancake.jpg',
+      url: require('../src/assets/img/pancake.jpg'),
       name: 'PANCAKES'
     },
     {
-      url: '../src/assets/img/rolls.jpg',
+      url: require('../src/assets/img/rolls.jpg'),
       name: 'ROLLS'
     },
     {
-      url: '../src/assets/img/scones.jpg',
+      url: require('../src/assets/img/scones.jpg'),
       name: 'SCONES'
     },
     {
-      url: '../src/assets/img/cake.jpg',
+      url: require('../src/assets/img/cake.jpg'),
       name: 'CAKES'
     },
     {
-      url: '../src/assets/img/candy.jpg',
+      url: require('../src/assets/img/candy.jpg'),
       name: 'CANDY'
     },
     {
-      url: '../src/assets/img/snacks.jpg',
+      url: require('../src/assets/img/snacks.jpg'),
       name: 'SNACKS'
     },
     {
-      url: '../src/assets/img/canning.jpg',
+      url: require('../src/assets/img/canning.jpg'),
       name: 'CANNING'
     },
     {
-      url: '../src/assets/img/casserole.jpg',
+      url: require('../src/assets/img/casserole.jpg'),
       name: 'CASSEROLES'
     },
     {
-      url: '../src/assets/img/cookies.jpg',
+      url: require('../src/assets/img/cookies.jpg'),
       name: 'COOKIES'
     },
     {
-      url: '../src/assets/img/brownies.jpg',
+      url: require('../src/assets/img/brownies.jpg'),
       name: 'BROWNIES'
     },
     {
-      url: '../src/assets/img/frosting.jpg',
+      url: require('../src/assets/img/frosting.jpg'),
       name: 'FROSTINGS'
     },
     {
-      url: '../src/assets/img/main.jpg',
+      url: require('../src/assets/img/main.jpg'),
       name: 'MAIN DISHES'
     },
     {
-      url: '../src/assets/img/side.jpg',
+      url: require('../src/assets/img/side.jpg'),
       name: 'SIDE DISHES'
     },
 
     {
-      url: '../src/assets/img/pie.jpg',
+      url: require('../src/assets/img/pie.jpg'),
       name: 'PIES & CRUSTS'
     },
     {
-      url: '../src/assets/img/soup.jpg',
+      url: require('../src/assets/img/soup.jpg'),
       name: 'SOUPS & SALADS'
     },
     {
-      url: '../src/assets/img/dips.jpg',
+      url: require('../src/assets/img/dips.jpg'),
       name: 'DIPS | SAUCES | DRESSINGS'
     },
     {
-      url: '../src/assets/img/pastries.jpg',
+      url: require('../src/assets/img/pastries.jpg'),
       name: 'DESSERTS | PASTRIES | PUDDINGS'
     },
     {
-      url: '../src/assets/img/misc.jpg',
+      url: require('../src/assets/img/misc.jpg'),
       name: 'MISC'
     }
 
--- cooper-cookbook.client/src/components/Categories.vue
+++ cooper-cookbook.client/src/components/Categories.vue
@@ -3,7 +3,7 @@
   <div class="categories rounded col-5 col-md-2 mx-2 my-4 px-0 card shadow">
     <router-link :to="{name:'RecipesPage', params:{categoryName: category.name}}">
       <div class="embed-responsive embed-responsive-4by3  d-flex justify-content-center align-items-end">
-        <img :src="url(category.url)" class="card-img-top embed-responsive-item" alt="image">
+        <img :src="category.url" class="card-img-top embed-responsive-item" alt="image">
         <div class=" col-12 p-1 card card-background">
           <h5 class="text-light my-2">
             {{ category.name }}
