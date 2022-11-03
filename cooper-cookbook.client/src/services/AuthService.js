0b7ca22eb84c2c9fa32299e8b8ebcc157c483cd9
Kayden Cooper <kayden_cooper@yahoo.com>
2022-11-03 12:56:57 -0600

    auth change

--- cooper-cookbook.client/src/services/AuthService.js
+++ cooper-cookbook.client/src/services/AuthService.js
@@ -7,41 +7,41 @@ import { api } from './AxiosService'
 import { recipeService } from './RecipeService'
 import { socketService } from './SocketService'
 
-export const AuthService = initialize({
-  domain,
-  clientId,
-  audience,
-  useRefreshTokens: true,
-  onRedirectCallback: appState => {
-    router.push(
-      appState && appState.targetUrl
-        ? appState.targetUrl
-        : window.location.pathname
-    )
-  }
-})
+// export const AuthService = initialize({
+//   domain,
+//   clientId,
+//   audience,
+//   useRefreshTokens: true,
+//   onRedirectCallback: appState => {
+//     router.push(
+//       appState && appState.targetUrl
+//         ? appState.targetUrl
+//         : window.location.pathname
+//     )
+//   }
+// })
 
-AuthService.on(AuthService.AUTH_EVENTS.AUTHENTICATED, async function() {
-  api.defaults.headers.authorization = AuthService.bearer
-  api.interceptors.request.use(refreshAuthToken)
-  //AppState.user = AuthService.user
-  //await accountService.getAccount()
-  socketService.authenticate(AuthService.bearer)
-  //await recipeService.getAll()
-  // NOTE if there is something you want to do once the user is authenticated, place that here
-})
+// AuthService.on(AuthService.AUTH_EVENTS.AUTHENTICATED, async function() {
+//   api.defaults.headers.authorization = AuthService.bearer
+//   api.interceptors.request.use(refreshAuthToken)
+//   //AppState.user = AuthService.user
+//   //await accountService.getAccount()
+//   socketService.authenticate(AuthService.bearer)
+//   //await recipeService.getAll()
+//   // NOTE if there is something you want to do once the user is authenticated, place that here
+// })
 
-async function refreshAuthToken(config) {
-  if (!AuthService.isAuthenticated) { return config }
-  const expires = AuthService.identity.exp * 1000
-  const expired = expires < Date.now()
-  const needsRefresh = expires < Date.now() + (1000 * 60 * 60 * 12)
-  if (expired) {
-    await AuthService.loginWithPopup()
-  } else if (needsRefresh) {
-    await AuthService.getTokenSilently()
-    api.defaults.headers.authorization = AuthService.bearer
-    socketService.authenticate(AuthService.bearer)
-  }
-  return config
-}
+// async function refreshAuthToken(config) {
+//   if (!AuthService.isAuthenticated) { return config }
+//   const expires = AuthService.identity.exp * 1000
+//   const expired = expires < Date.now()
+//   const needsRefresh = expires < Date.now() + (1000 * 60 * 60 * 12)
+//   if (expired) {
+//     await AuthService.loginWithPopup()
+//   } else if (needsRefresh) {
+//     await AuthService.getTokenSilently()
+//     api.defaults.headers.authorization = AuthService.bearer
+//     socketService.authenticate(AuthService.bearer)
+//   }
+//   return config
+// }
